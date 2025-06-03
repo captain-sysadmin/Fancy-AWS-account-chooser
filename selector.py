1f80b3dbd46a0121285d45af8f5e2be9598b9b82
Willem <willem.koopman@sonnedix.com>
2025-06-03 13:41:01 +0100

    seems like it works

--- selector.py
+++ selector.py
@@ -1,7 +1,22 @@
+
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
 from textual.widgets import ListView, ListItem, Static, Button
 from textual.binding import Binding
+
+import os
+import configparser
+import subprocess
+
+config = configparser.ConfigParser()
+aws_conf_path = os.path.expanduser("~/.aws/config")
+
+accounts = config.read(aws_conf_path)
+if accounts == []:
+    raise ValueError(f'Cannot find aws config at {aws_conf_path}')
+
+
+
 
 class TwoPaneApp(App):
     CSS = """
@@ -15,6 +30,9 @@ class TwoPaneApp(App):
         border: solid $primary;
         padding: 1;
     }
+    #main_content {
+        height: 100%;
+    }
     """
     
     BINDINGS = [
@@ -24,22 +42,54 @@ class TwoPaneApp(App):
     def __init__(self):
         super().__init__()
         self.data = {
-            "item1": "This is the content for item 1\nIt can be multiple lines",
-            "item2": "Content for item 2\nWith some more details here",
-            "item3": "Item 3 information\nAnother line\nAnd another",
-            "another_key": "Some other data\nWith multiple\nLines of text",
             "last_item": "Final item content"
         }
+        self.selected_aws_account = ""
+        self.load_aws_accounts()
+    def load_aws_accounts(self)->None:
+        '''This finds the local AWS config file and
+        tries to pull out the accounts that we can request 
+        ephemeral creds for
+        '''
+
+        config = configparser.ConfigParser()
+        aws_conf_path = os.path.expanduser("~/.aws/config")
+
+        accounts = config.read(aws_conf_path)
+        if accounts == []:
+            raise ValueError(f'Cannot find aws config at {aws_conf_path}')
+        for profile in config.sections():
+            nice_name = profile.replace('profile ', '')
+            if config[profile].get('sso_session', None):
+                self.data[nice_name] = f"Aws account ID: {config[profile].get('sso_account_id', 'broken')}"
+
+    def retrieve_creds(self, account_name:str)-> bool:
+        '''renew the credentials for a given account,
+        this uses subprocess, so might be a bit shit'''
+
+        print(f'fart {account_name}')
+        output = subprocess.run(f"aws sso login --profile {account_name}", shell=True,  capture_output=True)
+        if output.returncode == 0:
+            return True
+        else:
+            return False
+    def set_default_profile(self, account_name:str) -> None:
+        '''update the env and shell to point default account to which ever one we just
+        got creds for
+        '''
+        with open(os.path.expanduser("~/.aws_includes"), 'w') as fh:
+            fh.write(f'export AWS_DEFAULT_PROFILE={account_name}')
+
     
     def compose(self) -> ComposeResult:
         with Vertical():
-            yield Button("Click me!", id="my_button")
             with Horizontal():
                 # List pane
                 yield ListView(id="list_pane")
                 
                 # Info pane
                 yield Static("Select an item from the list", id="info_pane")
+            yield Button("Get AWS credentials", id="my_button")
     
     def on_mount(self) -> None:
         """Populate the list after mounting."""
@@ -57,8 +107,17 @@ class TwoPaneApp(App):
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Handle button press."""
         if event.button.id == "my_button":
+            list_view = self.query_one("#list_pane", ListView)
+            selected_item = list_view.highlighted_child
+            selected_account = selected_item.children[0].renderable
             info_pane = self.query_one("#info_pane", Static)
-            info_pane.update("Button was clicked!")
+            info_pane.update(f"Getting SSO credentials for {selected_account}, this will launch a browser window")
+            working = self.retrieve_creds(selected_account)
+            if not working:
+                info_pane.update('Could not get creds.')
+            else:
+                info_pane.update('WE HAVE CREDENTIONS')
+                self.set_default_profile(selected_account)
 
 if __name__ == "__main__":
     TwoPaneApp().run()
